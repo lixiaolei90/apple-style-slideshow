f82868789bcf64a32020eaf41858cb5df5fce5ff
Xiaolei Li <lixiaolei327@gmail.com>
2019-05-12 16:15:31 +0800

    add jquery

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
     var toWidth = 0;
-    var position = new Array();
+    var positions = new Array();
 
     $('#slides .slide').each(function(i) {
-
+        positions[i] = toWidth;
+        console.log(this);
+        toWidth += $(this).width();
+        console.log(toWidth);
     });
+
+    $('#slides').width(toWidth);
+
+    $('#menu ul li a').click(function(e) {
+        $('li.menuItem').removeClass('act').addClass('inact');
+        $(this).parent().addClass('act');
+
+        var pos = $(this).parent().prevAll('.menuItem').length;
+        $('#slides').stop().animate({marginLeft: -positions[pos] + 'px'}, 450);
+        
+        e.preventDefault();
+    });
+    $('#menu ul li.menuItem:first').addClass('act').siblings().addClass('inact');
 });
